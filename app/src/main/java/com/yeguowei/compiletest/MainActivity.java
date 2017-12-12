f3a23a8ae0929c108ef4f1a4fdf7369418157ab8
guowei <674567699@qq.com>
2017-12-12 11:17:21 +0800

    no message

--- app/src/main/java/com/yeguowei/compiletest/MainActivity.java
+++ app/src/main/java/com/yeguowei/compiletest/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.yeguowei.compilelibrarytest.CompileLibraryTest;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        String c = CompileLibraryTest.TEST;
     }
 
     @Override
