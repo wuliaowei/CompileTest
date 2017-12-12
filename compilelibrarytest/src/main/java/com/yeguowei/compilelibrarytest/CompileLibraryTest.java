c714e557c028fa92e4bf7d4acbc42601ce45effa
guowei <674567699@qq.com>
2017-12-12 11:00:54 +0800

    add Library!

---
+++ compilelibrarytest/src/main/java/com/yeguowei/compilelibrarytest/CompileLibraryTest.java
@@ -0,0 +1,13 @@
+package com.yeguowei.compilelibrarytest;
+
+/**
+ * Created by guowei on 2017/12/12.
+ */
+
+public class CompileLibraryTest {
+    public static final String TEST = "COMPILE_LIBRARY_TEST";
+
+    public String getName() {
+        return "CompileLibraryTest";
+    }
+}
